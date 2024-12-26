0183fa650ef5ebd7f70a510fb0926a5c32bb2fc7
AugustoCNO <gutocesar.c1@gmail.com>
2024-12-26 10:26:18 -0300

    criando  a pagina de administrador

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
-# Register your models here.
+
+class Estudantes(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
+    list_display_links = ['id', 'nome']
+    list_per_page = 20
+    search_fields = ['nome']
+
+admin.site.register(Estudante, Estudantes)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ['id', 'curso', 'descricao', 'nivel']
+    list_display_links = ['id', 'curso']
+    search_fields = ['curso']
+
+admin.site.register(Curso, Cursos)
